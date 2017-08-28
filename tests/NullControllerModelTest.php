254f5fae1e1faa8abbc665d66ec5fd226c73bc87
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-28 10:12:58 -0400

    Create NullControllerModelTest.php

---
+++ tests/NullControllerModelTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use SymfonyUtil\Component\HttpFoundation\NullControllerModel;
+
+/**
+ * @covers \SymfonyUtil\Component\HttpFoundation\NullControllerModel
+ */
+final class NullControllerModelTest extends TestCase
+{
+    public function testCanBeCreated()
+    {
+        $this->assertInstanceOf(
+            // ::class, // 5.4 < php
+            'SymfonyUtil\Component\HttpFoundation\NullControllerModel',
+            new NullControllerModel();
+        );
+    }
+
+    public function testReturnsArray()
+    {
+        $this->assertInternalType('array', (new NullControllerModel()))->__invoke(new Request);
+    }
+}
