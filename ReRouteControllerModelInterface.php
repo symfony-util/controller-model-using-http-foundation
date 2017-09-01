bc84055ec90b46028bb264d3cba9eda96fa4dcb1
Nardberjean <Nardberjean@users.noreply.github.com>
2017-09-01 06:42:30 -0400

    Update ReRouteControllerModelInterface.php

--- ReRouteControllerModelInterface.php
+++ ReRouteControllerModelInterface.php
@@ -21,12 +21,13 @@ interface ReRouteControllerModelInterface
     /**
      * Returns ResponseParameters to the given route with the given parameters.
      *
-     * @param string $route      The name of the route
-     * @param mixed  $parameters An array of parameters
+     * @param string  $route      The name of the route
+     * @param mixed   $parameters An array of parameters
+     * @param Request $request    May be used in some redirecting implementations for eg. browser detection
      *
      * @return ResponseParameters
      */
-    public function __invoke($route, $parameters = []);
+    public function __invoke($route, $parameters = [], Request $request = null);
 }
 
 // Inspired from https://github.com/symfony/symfony/blob/v3.3.8/src/Symfony/Bundle/FrameworkBundle/Controller/ControllerTrait.php
