17495644bbbd62b9bb024ed7be38c5069db9a998
Nardberjean <Nardberjean@users.noreply.github.com>
2018-03-05 07:47:08 -0500

    Update ReRouteControllerModel.php

--- ReRouteControllerModel.php
+++ ReRouteControllerModel.php
@@ -42,7 +42,7 @@ class ReRouteControllerModel implements ReRouteControllerModelInterface
      */
     public function __invoke($routeName, $parameters = [], Request $request = null)
     {
-        return new ResponseParameters([], $this->reRoute($routeName, $parameters));
+        return new ResponseParameters([], ($this->reRoute)($routeName, $parameters));
     }
 }
 
